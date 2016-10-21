aa6a7ec64646e317f25d563857bb53b11d6a9436
Michael Kurowski <Byamarro@gmail.com>
2016-10-21 18:32:02 +0200

    Small refactor of loadAssets. Preparing for robust assets loading engine  	modified:   src/js/assetsLoader.js

--- src/js/assetsLoader.js
+++ src/js/assetsLoader.js
@@ -12,17 +12,27 @@ let assets = {
 function loadAssets(){
 	//TODO: reading assets config from assets/config.json instead of from var
 	//Loads images
-	
-	assetsConfig.images.forEach( (element, index, arr) => {
-		let asset = new Image();
-		asset.src = element;
-		assets.imagesReadiness[index] = new Promise ( (resolve, reject) => {
-			asset.onload = (ev) => {
-				resolve();
-			}
-		});
-		assets.images[index] = asset;
-	});
 
-	return Promise.all(assets.imagesReadiness);
+	//load images
+	//TODO rewrite function to serve as just universal assets loader 
+	function loadImages() {
+		assetsConfig.images.forEach( (element, index, arr) => {
+			var imagesPromises = [];
+			let asset = new Image();
+			asset.src = element;
+			imagesPromises.push(
+					new Promise ( (resolve, reject) => {
+					asset.onload = (ev) => {
+						resolve();
+					}
+				});
+			);
+			assets.images[index] = asset;
+		});
+		return imagesPromises;
+	};
+	//Go to next step after all images will be loaded
+	Promise.all(  loadImages()  )
+		.then( (value, error) => {
+	})
 }
